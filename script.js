8cf09480abb678b6648e86c7b5a1a403edb99025
blevinspercussion <blevinspercussion@gmail.com>
2023-02-11 19:28:10 -0500

    Working on projects sub-menu

--- script.js
+++ script.js
@@ -1,3 +1,32 @@
+//////////////////////////////////////////////
+// Define custom elements/web components /////
+//////////////////////////////////////////////
+
+class DesktopHeader extends HTMLElement {
+    constructor() {
+        super();
+    }
+    connectedCallback() {
+        this.innerHTML = `
+        <header>
+        <img class="logo" src="./media/images/AdamBCodesLogo.png" alt="" />
+        <ul class="menu-hor">
+            <li><a href="#webdev-projects">Projects</a></li>
+            <li><a href="./tutoring.html">Tutoring</a></li>
+            <li><a href="./about.html">About Me</a></li>
+            <li><a href="./contact.html">Contact</a></li>
+        </ul>
+        </header>
+      `
+    }
+}
+
+// Define custom elements
+customElements.define("desktop-header", DesktopHeader);
+
+
+
+
 body = document.querySelector('body');
 
 introPic = document.getElementById('intro-pic');
